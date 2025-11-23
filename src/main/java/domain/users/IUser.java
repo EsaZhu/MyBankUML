1fcf4785011dee212d7fb5d30e880cdf70e57199
Alisa <aignatina@gmail.com>
2025-11-23 02:55:39 -0500

    IUser, BankTellerAccount, Branch draft

--- src/main/java/domain/users/IUser.java
+++ src/main/java/domain/users/IUser.java
@@ -1,6 +1,11 @@
 package domain.users;
 
+import domain.enums.UserRole;
 
 public interface IUser {
- 
+    String getUsername();
+
+    String getPasswordHash();
+
+    UserRole getRole();
 }
--- src/main/java/domain/users/UserAccount.java
+++ src/main/java/domain/users/UserAccount.java
@@ -1,5 +1,15 @@
 package domain.users;
 
 public class UserAccount {
+    private String userID;
+    
+
+    public UserAccount(String userID) {
+        this.userID = userID;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
     
 }
