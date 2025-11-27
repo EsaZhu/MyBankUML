720048ce996f246d90abfdc33f434f36319e5e40
Sila <silabk13@gmail.com>
2025-11-27 17:28:37 -0500

    Fix banktest

--- src/test/java/unit_tests/BankTest.java
+++ src/test/java/unit_tests/BankTest.java
@@ -22,17 +22,7 @@ public class BankTest {
     void setUp() {
         fakeDb = new FakeDatabase();
         resultList = new ArrayList<>();
-        bank = new Bank("Test Bank", "B100", resultList, fakeDb);
-    }
-
-    // Test addBranch
-    @Test
-    void testAddBranch() {
-        Branch b = new Branch("BR1", "Main Branch", "Test Bank");
-        bank.addBranch(b);
-
-        assertTrue(fakeDb.addedBranches.contains(b));
-        assertTrue(fakeDb.updatedBanks.contains("B100"));
+        bank = new Bank("Test Bank", "B100", fakeDb);
     }
 
     // Test getBranches
@@ -44,44 +34,11 @@ public class BankTest {
         fakeDb.savedBranches.put("BR1", b1);
         fakeDb.savedBranches.put("BR2", b2);
 
-        bank.addBranch(b1);
-        bank.addBranch(b2);
-
         ArrayList<Branch> list = bank.getBranches();
 
         assertEquals(2, list.size());
         assertTrue(list.contains(b1));
         assertTrue(list.contains(b2));
-    }
-
-    // Test searchByID 
-    @Test
-    void testSearchByID() {
-        IUser user = new UserAccount("U1", "John", null, null, null, null, null);
-        fakeDb.userByID = user;
-
-        IUser result = bank.searchByID("U1");
-
-        assertEquals(user, result);
-        assertEquals(1, bank.resultList.size());
-        assertEquals(user, bank.resultList.get(0));
-    }
-
-    // Test searchByAttribute 
-    @Test
-    void testSearchByAttribute() {
-        IUser u1 = new UserAccount("U1", "John", null, null, null, null, null);
-        IUser u2 = new UserAccount("U2", "Tim", null, null, null, null, null);
-
-        ArrayList<IUser> users = new ArrayList<>();
-        users.add(u1);
-        users.add(u2);
-        fakeDb.attributeResults = users;
-
-        ArrayList<IUser> results = bank.searchByAttribute("name", "John");
-
-        assertEquals(2, results.size());
-        assertEquals(results, bank.resultList);
     }
 
     // Database stub
